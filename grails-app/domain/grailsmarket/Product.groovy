803a35c54c514e79c1cf9d5bade9e22c65e93638
HemrajRijal <her0139@my.londonmet.ac.uk>
2017-07-26 21:08:27 +0545

    Add: domain Product

---
+++ grails-app/domain/grailsmarket/Product.groovy
@@ -0,0 +1,21 @@
+package grailsmarket
+
+class Product {
+    String name
+    double price
+    int quantity
+    Category category
+    String status
+    String serialNumber
+    String manufacturer
+    Byte[] image
+
+    static constraints = {
+        name(blank: false, nullable: false)
+        quantity(blank: false, nullable: false)
+        status(blank: false, nullable: false)
+        serialNumber(blank: false, nullable: false)
+        manufacturer(blank: false, nullable: false)
+        category()
+    }
+}
